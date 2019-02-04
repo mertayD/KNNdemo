f810ca16e37ddc88af471b74cc030eeda3c431b4
mertayd <md2289@nau.edu>
2019-02-04 16:23:39 -0700

    Eigen Commit

--- src/knn.cpp
+++ src/knn.cpp
@@ -8,5 +8,29 @@ int knn(
   double *test_predictions_ptr //mac_neighbors
 )
 {
+  Eigen::Map <Eigen::MatrixXd > train_inputs_mat((double*)train_inputs_ptr, n_observations, n_features);
+  Eigen::Map< Eigen::VectorXd > test_input_vec(
+    (double*)test_input_ptr, n_features);
+  Eigen::VectorXd dist_vec(n_observations);
+  Eigen::VectorXi sorted_index_vec(n_observations);
+  for(int i = 0; i< n_observations; i++){
+    dist_vec(i) = (train_inputs_mat.row(i).transpose() - test_input_vec).norm();
+    sorted_index_vec(i) = i;
+  }
+  std::sort(
+    sorted_index_vec.data(),
+    sorted_index_vec.data()+sorted_index_vec.size(),
+    [&dist_vec](int left, int right){
+      return dist_vec(left) < dist_vec(right);
+    }
+  );
+  double total = 0.0;
+  for(int model_i = 0; model_i<max_neighbours; model_i++)
+  {
+    int neighbors = model_i + 1;
+    int row_i = sorted_index_vec(model_i);
+    total += train_label_ptr[row_i];
+    test_predictions_ptr[model_i] = total/neighbors;
+  }
   return 0;
 }
