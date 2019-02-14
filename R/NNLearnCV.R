c8150561ba43f33d98c686367cba238bef7cc372
shortykg <swtkaitlyn@gmail.com>
2019-02-14 12:59:00 -0700

    documentation except for examples is finished

--- R/NNLearnCV.R
+++ R/NNLearnCV.R
@@ -1,14 +1,35 @@
 #' Nearest Neighbors Learning Algorithm
 #'
-#'   explination
+#'   This is a learning algorithm that uses cross-validation 
+#'   to select the number of neighbors that minimizes the mean
+#'   validation loss
 #'
 #' @param X.mat a training data set
 #' @param y.vec a training data set
-#' @param max.neighbors=30 The max number of neighbors to fin the best k in nearest neighbors
-#' @param fold.vec=NULL is a vector of fold ID numbers. If fold.vec is NULL, randomly assign fold IDs from 1 to n.folds,
+#' @param max.neighbors=30 The max number of neighbors to fin the best k
+#'                         in nearest neighbors
+#' @param fold.vec=NULL is a vector of fold ID numbers. If fold.vec is NULL
+#'                      randomly assign fold IDs from 1 to n.folds
 #' @param n.folds=5 is the number of folds used to compute error
 #'
-#' @return returnList
+#' @return returnList a list containing:
+#'         X.mat - training data
+#'         y.vec - training data
+#'         train.loss.mat - matrice of loss values for each fold and number
+#'           of neighbors
+#'         validation.loss.mat - matrice of loss values for each fold and
+#'            number of neighbors
+#'         train.loss.vec - vector with max.neighbors elements: 
+#'            mean loss over all folds
+#'         validation.loss.vec - vector with max.neighbors elements: 
+#'            mean loss over all folds
+#'         selected.neighbors - number of neighbors selected by minimizing 
+#'            the mean validation loss
+#'         predict(testX.mat) - a function that takes a matrix of 
+#'            inputs/features and returns a vector of predictions. 
+#'            It should check the type/dimension of testX.mat and stop() 
+#'            with an informative error message if there are any issues.
+#'         
 #' @export
 #' 
 #' @examples
